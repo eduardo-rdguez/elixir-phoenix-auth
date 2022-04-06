c7fe2d66e7ff3d9b6e8980b2920757358a08fb8a
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-06 11:23:19 -0500

    Create auth pipeline for guardian module

---
+++ lib/elixir_phoenix_auth_web/auth_pipeline.ex
@@ -0,0 +1,10 @@
+defmodule ElixirPhoenixAuthWeb.AuthPipeline do
+  use Guardian.Plug.Pipeline,
+    otp_app: :elixir_phoenix_auth,
+    module: ElixirPhoenixAuth.Guardian,
+    error_handler: ElixirPhoenixAuth.AuthErrorHandler
+
+  plug Guardian.Plug.VerifyHeader, scheme: "Bearer"
+  plug Guardian.Plug.EnsureAuthenticated
+  plug Guardian.Plug.LoadResource
+end
