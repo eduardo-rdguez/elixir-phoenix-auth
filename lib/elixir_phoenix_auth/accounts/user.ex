2d7af1cd20131524c844a1abc79130d6c9cb59a8
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-05 20:31:58 -0500

    Import hashpwsalt at the top of user schema

--- lib/elixir_phoenix_auth/accounts/user.ex
+++ lib/elixir_phoenix_auth/accounts/user.ex
@@ -2,7 +2,7 @@ defmodule ElixirPhoenixAuth.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Comeonin.Bcrypt
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
   schema "users" do
     field :email, :string
@@ -30,7 +30,7 @@ defmodule ElixirPhoenixAuth.Accounts.User do
   defp put_password_hash(changeset) do
     case changeset do
       %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
-        put_change(changeset, :password_hash, Bcrypt.hashpwsalt(pass))
+        put_change(changeset, :password_hash, hashpwsalt(pass))
 
       _ ->
         changeset
