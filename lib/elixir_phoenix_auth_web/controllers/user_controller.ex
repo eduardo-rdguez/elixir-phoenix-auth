435f656621b9d91d89eab31cafb5cb93ee48adc2
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-07 23:10:20 -0500

    Update show function to get the current user claims in JWT token

--- lib/elixir_phoenix_auth_web/controllers/user_controller.ex
+++ lib/elixir_phoenix_auth_web/controllers/user_controller.ex
@@ -19,9 +19,9 @@ defmodule ElixirPhoenixAuthWeb.UserController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
-    render(conn, "show.json", user: user)
+  def show(conn, _params) do
+    user = Guardian.Plug.current_resource(conn)
+    render(conn, "user.json", user: user)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
